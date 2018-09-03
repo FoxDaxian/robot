785928fa229825797c720b89c470ab6b08831010
冯世雨 <fengshiyu02@meituan.com>
2018-09-03 19:58:20 +0800

    update: 测试jinjia2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # robot
 
 * 使用pipenv来管理python包
+
+
+## TODO
+* 利用make模拟 npm run dev npm run build?
--- index.py
+++ index.py
@@ -11,13 +11,20 @@
 # r = requests.post("http://openapi.tuling123.com/openapi/api/v2", data=payload)
 # print(r.text)
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return '你123好啊'
+    return render_template('test.html', name='12冯世雨')
+
+
+@app.route('/user/<name>')
+def showUserName(name):
+    return '用户名: %s' % name
+
 
 if __name__ == '__main__':
     app.config.from_object('flaskCfg')
